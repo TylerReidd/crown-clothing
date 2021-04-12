9d9149bf2c5c5edbc9398d8d4a32d6d8a21d3d77
Tyler Reid <Tylerreid1988@gmail.com>
2021-04-11 22:37:57 -0400

    add signin page and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SigninSignup from './pages/signin-signup/signin-signup.component'
 // const HatsPage = () => (
 //   <div>
 //     <h1>HATS PAGE</h1>
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SigninSignup} />
       </Switch>
     </div>
   );
