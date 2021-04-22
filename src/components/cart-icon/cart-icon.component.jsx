71380cf79ef43c30970e628c070f6af6ccf31dc4
Tyler Reid <Tylerreid1988@gmail.com>
2021-04-22 15:25:48 -0400

    updated app with reselect library replacing destructured values with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import { toggleCartHidden } from '../../components/redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../components/redux/cart/cart.selectors';
 
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+import  { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag2.svg'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'></span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
@@ -18,7 +19,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
   )(CartIcon);
